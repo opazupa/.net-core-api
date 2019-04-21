eea39aa30ae6aa9821fea3f37ab8921d1a02201a
Olli Saarinen <>
2019-04-21 20:31:55 +0300

    Prettified xml documentation

--- API/Controllers/CodingSkillController.cs
+++ API/Controllers/CodingSkillController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 using FeatureLibrary.Models;
 using API.FilterModels;
+using Microsoft.AspNetCore.Http;
 
 namespace API.Controllers
 {
@@ -18,9 +19,9 @@ namespace API.Controllers
         /// </summary>
         /// <param name="filter"></param>
         /// <returns>Coding skills matching to filter values.</returns>
-        /// <response code="200">Found coding skills.</response>
-        /// <response code="204">If no skills are found.</response> 
         [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<CodingSkill>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(object), StatusCodes.Status204NoContent),]
         public ActionResult<IEnumerable<CodingSkill>> Get([FromQuery] CodingSkillFilter filter)
         {
             return new CodingSkill[] { };
@@ -31,9 +32,9 @@ namespace API.Controllers
         /// </summary>
         /// <param name="id"></param>
         /// <returns>Found coding skill.</returns>
-        /// <response code="200">Found coding skill with the id.</response>
-        /// <response code="404">If no skills are found with the id.</response> 
         [HttpGet("{id}")]
+        [ProducesResponseType(typeof(CodingSkill), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(object), StatusCodes.Status404NotFound)]
         public ActionResult<CodingSkill> Get(int id)
         {
             return new CodingSkill();
@@ -42,22 +43,11 @@ namespace API.Controllers
         /// <summary>
         /// Post new coding skill.
         /// </summary>
-        /// <remarks>
-        /// Sample request:
-        ///
-        ///     POST /coding-skill
-        ///     {
-        ///        "id": 1,
-        ///        "name": "ASP.NET Core",
-        ///        "level": 3
-        ///     }
-        ///
-        /// </remarks>
         /// <param name="skill"></param>
-        /// <returns>A newly created coding skill item</returns>
-        /// <response code="201">Returns the newly created coding skill</response>
-        /// <response code="400">If the item is null or invalid.</response> 
+        /// <returns>The newly created coding skill item</returns>
         [HttpPost]
+        [ProducesResponseType(typeof(CodingSkill), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public void Post([FromBody] CodingSkill skill)
         {
         }
@@ -65,22 +55,12 @@ namespace API.Controllers
         /// <summary>
         /// Update the given coding skill.
         /// </summary>
-        /// <remarks>
-        /// Sample request:
-        ///
-        ///     Put /coding-skill/1
-        ///     {
-        ///        "name": "ASP.NET Core",
-        ///        "level": 5
-        ///     }
-        ///
-        /// </remarks>
         /// <param name="id"></param>
         /// <param name="skill"></param>
         /// <returns> The updated coding skill item</returns>
-        /// <response code="200">Returns the updated created coding skill</response>
-        /// <response code="400">If the item is null or invalid.</response> 
         [HttpPut("{id}")]
+        [ProducesResponseType(typeof(CodingSkill), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(object), StatusCodes.Status404NotFound)]
         public void Put(int id, [FromBody] CodingSkill skill)
         {
         }
@@ -89,9 +69,10 @@ namespace API.Controllers
         /// Delete the coding skill.
         /// </summary>
         /// <param name="id"></param>
-        /// <response code="200">Coding skill deleted</response>
-        /// <response code="404">If the coding skill is not found.</response> 
+        /// <returns></returns>
         [HttpDelete("{id}")]
+        [ProducesResponseType(typeof(object), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(object), StatusCodes.Status404NotFound)]
         public void Delete(int id)
         {
         }
