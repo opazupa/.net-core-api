7524ff5f812a3eb02bfe715b53c5372cc9f8eb01
Olli Saarinen <>
2019-04-24 21:06:42 +0300

    Added in memory tests for repository layer :)

---
+++ Tests/UnitTests/Tests/Repositories/CodingSkillRepositoryTest.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using FeatureLibrary.Models;
+using FeatureLibrary.Repositories;
+using UnitTests.Utils.Setup;
+using Xunit;
+
+namespace UnitTests.Services
+{
+    public class CodingSkillRepositoryTest
+    {
+        private readonly IEnumerable<CodingSkill> testSkills = new List<CodingSkill>()
+        {
+            new CodingSkill(){Id = 1, Level = CodingSkillLevel.Ok, Name = "Python" },
+            new CodingSkill(){Id = 2, Level = CodingSkillLevel.Master, Name = "Typescript" }
+        };
+
+
+        [Fact]
+        public async Task GetSkillById()
+        {
+            using (var ctx = await DBContextHelper.ResetWithData(testSkills))
+            {
+                ICodingSkillRepository repo  = new CodingSkillRepository(ctx);
+
+                CodingSkill skill = await repo.GetById(testSkills.First().Id);
+
+                Assert.Equal(skill.Id, testSkills.First().Id);
+                Assert.Equal(skill.Name, testSkills.First().Name);
+                Assert.Equal(skill.Level, testSkills.First().Level);
+            }
+        }
+
+        [Fact]
+        public async Task GetSkillByLevel()
+        {
+            using (var ctx = await DBContextHelper.ResetWithData(testSkills))
+            {
+                ICodingSkillRepository repo = new CodingSkillRepository(ctx); var filter = new CodingSkillFilter()
+
+                {
+                    Levels = new List<CodingSkillLevel>() { CodingSkillLevel.Master, CodingSkillLevel.GettingThingsDone }
+                };
+                IEnumerable<CodingSkill> skills = await repo.GetByFilter(filter);
+
+                Assert.All(skills, skill => filter.Levels.Contains(skill.Level));
+                Assert.Equal(skills.First().Name, testSkills.ElementAt(1).Name); // Typescript
+            }
+        }
+    }
+}
---
+++ Tests/UnitTests/Utils/Setup/DBContextHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FeatureLibrary.Database;
+using FeatureLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace UnitTests.Utils.Setup
+{
+    public static class DBContextHelper
+    {
+        /// <summary>
+        /// Resets the in memory db with given data.
+        /// </summary>
+        /// <returns>In memory db context</returns>
+        /// <param name="data"></param>
+        public async static Task<FeatureContext> ResetWithData(IEnumerable<CodingSkill> data)
+        {
+            var ctx = Init();
+            await ctx.Database.EnsureDeletedAsync();
+            await ctx.AddRangeAsync(data);
+            await ctx.SaveChangesAsync();
+            return ctx;
+        }
+
+        /// <summary>
+        /// Init in memory db options.
+        /// </summary>
+        /// <returns></returns>
+        private static FeatureContext Init()
+        {
+            var dbOptions = new DbContextOptionsBuilder<FeatureContext>()
+                                .UseInMemoryDatabase(databaseName: new Guid().ToString())
+                                .Options;
+            return new FeatureContext(dbOptions);
+        }
+    }
+}
