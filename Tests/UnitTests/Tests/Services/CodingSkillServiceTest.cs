d880e8c4e015e98a1bbbb30b088fcb0b1526b953
Olli Saarinen <>
2019-04-24 20:09:35 +0300

    Added sample unit test for service method

---
+++ Tests/UnitTests/Tests/Services/CodingSkillServiceTest.cs
@@ -0,0 +1,26 @@
+using System.Threading.Tasks;
+using CoreLibrary.Exceptions;
+using FeatureLibrary.Models;
+using FeatureLibrary.Repositories;
+using FeatureLibrary.Services;
+using Moq;
+using Xunit;
+
+namespace UnitTests.Services
+{
+    public class CodingSkillServiceTest
+    {
+
+        [Fact]
+        public async Task AddCodingSkillWithNoLevel()
+        {
+            var toAdd = new CodingSkill() { Name = "Angular", Id = 3 };
+            var _repository = new Mock<ICodingSkillRepository>();
+            _repository.Setup(x => x.Add(null)).Returns(Task.FromResult(toAdd));
+
+            var codingSkillService = new CodingSkillService(_repository.Object);
+
+            await Assert.ThrowsAsync<BadRequestException>(() => codingSkillService.Add(toAdd));
+        }
+    }
+}
